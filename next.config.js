b37b223a05a9890233c6308a6e8bec9ceda5fbf1
Igor Gassmann <igor@igassmann.me>
2023-01-19 09:37:47 +0200

    Reproduce type error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withRoutes = require("nextjs-routes/config")();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withRoutes(nextConfig);
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -9,5 +9,9 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (typeof req.query.name !== 'string') {
+    return;
+  }
+
+  res.status(200).json({ name: req.query.name })
 }
